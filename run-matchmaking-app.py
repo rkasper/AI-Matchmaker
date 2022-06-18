d6e7c8a96e76b709f18e7b46af77884d315c7d60
Richard Kasperowski <r@kasperowski.com>
2022-06-18 10:51:36 -0400

    Move to top-level directory

--- run-matchmaking-app.py
+++ run-matchmaking-app.py
@@ -1,6 +1,8 @@
+import os
 import sys
 from streamlit import cli as stcli
 
 if __name__ == '__main__':
+    os.chdir("AI-Dating-App")
     sys.argv = ["streamlit", "run", "MatchMaking-App.py", "--global.disableWatchdogWarning", "true", "--server.headless", "true", "--server.port", "8080"]
     sys.exit(stcli.main())
