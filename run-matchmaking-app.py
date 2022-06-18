509e3cdd41740fb869ffd99ea7249e6c2afc8684
Richard Kasperowski <r@kasperowski.com>
2022-06-18 10:58:08 -0400

    Debug with print statements

--- run-matchmaking-app.py
+++ run-matchmaking-app.py
@@ -3,6 +3,10 @@ import sys
 from streamlit import cli as stcli
 
 if __name__ == '__main__':
+    print("--- We are in run-matchmaking-app.py. ---")
+    print("--- changing dir")
     os.chdir("AI-Dating-App")
+    print("--- Running the app")
     sys.argv = ["streamlit", "run", "MatchMaking-App.py", "--global.disableWatchdogWarning", "true", "--server.headless", "true", "--server.port", "8080"]
     sys.exit(stcli.main())
+    print("--- Done")
